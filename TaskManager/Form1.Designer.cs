14c4c277755f3961f9890f37e20531a03ddb39ee
joaodacoreg.io <oinaagua6@gmail.com>
2025-02-17 22:04:02 -0300

    designer UI

--- TaskManager/Form1.Designer.cs
+++ TaskManager/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TaskManager
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -15,7 +15,7 @@
         {
             if (disposing && (components != null))
             {
-                printf("test");
+ 
                 components.Dispose();
             }
             base.Dispose(disposing);
@@ -29,10 +29,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1085, 663);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- TaskManager/Form1.cs
+++ TaskManager/Form1.cs
@@ -1,20 +1,99 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TaskManager
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        private Panel sideMenu;
+        private Panel mainPanel;
+        private ListView taskList;
+
+        public MainForm()
         {
             InitializeComponent();
+            CustomizeUI();
+        }
+
+        private void CustomizeUI()
+        {
+            this.Text = "Task Manager";
+            this.Size = new Size(1000, 700);
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.BackColor = Color.White;
+
+            AddSideMenu();
+            AddMainPanel();
+            AddTaskList();
+        }
+
+        private void AddSideMenu()
+        {
+            sideMenu = new Panel()
+            {
+                Size = new Size(200, this.Height),
+                BackColor = Color.FromArgb(33, 37, 41), // Bootstrap dark
+                Dock = DockStyle.Left
+            };
+            this.Controls.Add(sideMenu);
+
+            AddButtons();
+        }
+
+        private void AddMainPanel()
+        {
+            mainPanel = new Panel()
+            {
+                Size = new Size(780, this.Height),
+                Location = new Point(200, 0),
+                BackColor = Color.White
+            };
+            this.Controls.Add(mainPanel);
+        }
+
+        private void AddButtons()
+        {
+            Button btnAddTask = CreateButton("+ Nova Tarefa", new Point(10, 20));
+            Button btnRemoveTask = CreateButton("- Remover Tarefa", new Point(10, 70));
+            Button btnUpdateTask = CreateButton("⟳ Atualizar Tarefa", new Point(10, 120));
+
+            sideMenu.Controls.Add(btnAddTask);
+            sideMenu.Controls.Add(btnRemoveTask);
+            sideMenu.Controls.Add(btnUpdateTask);
+        }
+
+        private Button CreateButton(string text, Point location)
+        {
+            Button button = new Button()
+            {
+                Text = text,
+                Font = new Font("Segoe UI", 10, FontStyle.Bold),
+                Size = new Size(180, 40),
+                Location = location,
+                ForeColor = Color.White,
+                BackColor = Color.FromArgb(13, 110, 253), // Bootstrap primary
+                FlatStyle = FlatStyle.Flat
+            };
+            button.FlatAppearance.BorderSize = 0;
+            return button;
+        }
+
+        private void AddTaskList()
+        {
+            taskList = new ListView()
+            {
+                View = View.Details,
+                Size = new Size(750, 600), // Tamanho estático
+                Location = new Point(20, 20),
+                Font = new Font("Segoe UI", 10),
+                FullRowSelect = true,
+                HeaderStyle = ColumnHeaderStyle.Clickable
+            };
+            taskList.Columns.Add("Nome", 300);
+            taskList.Columns.Add("Data", 200);
+            taskList.Columns.Add("Status", 150);
+            mainPanel.Controls.Add(taskList);
         }
     }
 }
